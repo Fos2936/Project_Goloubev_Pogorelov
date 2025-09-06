49bcc150bb4b42d36bd860b2befa3370e1c2d8e2
Fos2936 <Illdian2936@gmail.com>
2025-09-06 15:47:42 +0300

    New project

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,26 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val str = "A"
+    val symbolsArray = str.toCharArray()
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    for (i in symbolsArray) print(i)
+    println()
+    var count = 1
+    for (i in 0 .. symbolsArray.size - 1) {
+        if (i == symbolsArray.size - 1) {
+            print(symbolsArray[i])
+            print(count)
+            break;
+        }
+        if (symbolsArray[i] == symbolsArray[i + 1]) {
+            count++
+        }
+        else {
+            print(symbolsArray[i])
+            if (count == 1) {
+                continue
+            }
+            print(count)
+            count = 1
+        }
+    }
 }
